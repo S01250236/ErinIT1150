d9730d1be57cd0234dfa11ab6ab0fc24fbe370c4
S01250236 <70147827+S01250236@users.noreply.github.com>
2021-02-01 10:02:24 -0500

    Add files via upload

---
+++ Addition Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Addition_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            SayHi();
+            Console.ReadLine();
+
+
+
+        }
+
+        static void SayHi()
+        {
+
+            double x;
+            double y;
+            double z;
+
+            x = 10;
+            y = 6;
+            z = x + y; 
+
+
+            Console.Write("Z is: ");
+            Console.Write(z);
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+
+}
+
+
